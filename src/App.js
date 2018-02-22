b9f5606c4b12026022085717a920fbf7901d62c1
Varun Munjeti <vrunjeti@gmail.com>
2018-02-22 05:15:47 -0600

    react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './styles/App.css'
+import { Grid, Navbar, Jumbotron } from 'react-bootstrap'
+import './styles/app.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">Hi</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
+        <Jumbotron className="banner">
+          <Grid>
+            <h1>Hola</h1>
+          </Grid>
+        </Jumbotron>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
