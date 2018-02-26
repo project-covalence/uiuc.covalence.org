9920974c88cb040385ffcda0eeef7fd342a55d5e
Varun Munjeti <vrunjeti@gmail.com>
2018-02-26 05:07:51 -0600

    app and navbar components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
-import App from './App'
+import App from './App.component'
+import Navbar from './Navbar.component'
+
 import Colors from './Colors.container'
 import StuffList from './StuffList.container'
 
-export { App, Colors, StuffList }
+export { App, Colors, Navbar, StuffList }
--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Route } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
-import { Grid, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 
 import configureStore, { history } from './store/configureStore'
-import { App, Colors } from './components'
+import { App, Colors, Navbar } from './components'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
 
@@ -20,16 +18,7 @@ ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <Link to="/">Hi</Link>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
+        <Navbar />
         <Route exact path="/" component={App} />
         <Route path="/colors" component={Colors} />
       </div>
