1afb5db426a8169ec428edc2a9f9a32428a37253
Varun Munjeti <vrunjeti@gmail.com>
2018-02-26 05:19:44 -0600

    moved mock fetch into utils

---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+import type { Stuff } from './../types'
+
+function mockFetchStuff(): Promise<{ stuff: Stuff }> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ stuff: ['thing1', 'thing2', 'thing3'] })
+    }, Math.random() * 50)
+  })
+}
+
+export default {
+  mockFetchStuff
+}
